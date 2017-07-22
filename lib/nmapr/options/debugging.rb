10df7177a0d1caf8913673d628928349bfbec0c6
Kent Gruber <kgruber1@emich.edu>
2017-07-22 16:15:51 -0400

    Update debugging, add a todo in it.

--- lib/nmapr/options/debugging.rb
+++ lib/nmapr/options/debugging.rb
@@ -1,18 +1,23 @@
 module Nmapr
   module Options
     module Debugging 
+      # refactor to work like verbose / combine in to Verbose
       def debug
         add_argument("-d")
       end
       def open
         add_argument("--open")
       end
+      alias show_open open
       def packet_trace 
         add_argument("--packet-trace")
       end
       def reason 
         add_argument("--reason")
       end
+      def list_interfaces
+        add_argument("--iflist")
+      edn
     end
   end
 end
